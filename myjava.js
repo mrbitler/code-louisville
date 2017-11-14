34f32eea765eacec35ebbbb43bc66daa80b98c92
mrbitler <32917412+mrbitler@users.noreply.github.com>
2017-11-14 18:05:36 -0500

    Update myjava.js

--- myjava.js
+++ myjava.js
@@ -1,12 +1,12 @@
-
+// Function for newsletter:
     function openWin() {
        window.alert("First one will arrive soon, thanks") 
          }
- 
+// Function for feedback link:
 function myFunction() {
     alert("The form was submitted");
 }
-
+// Function for go back button:
 function goBack() {
     window.history.back();
 }
